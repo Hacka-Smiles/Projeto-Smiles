028a580da9f6495da11cb920670487308dfdb571
gabseiler <g.seilerspada@usp.br>
2021-03-27 17:50:57 -0300

    páginas

---
+++ web/pages/cadastrar/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import db from '../../db.json';
+import Button from '../../src/components/Button';
+
+export default function Cadastrar() {
+  return (
+    <ThemeProvider theme={db.theme}>
+      {/* <QuizScreen
+        externalQuestions={db.questions}
+        externalBg={db.bg}
+      /> */}
+      <Button>Login</Button>
+    </ThemeProvider>
+  );
+}
