18b454cc28d1dfe77b6ccf49078362e00a1fb73f
ViniciusSerafim <vinicmatheus@usp.br>
2021-03-28 18:19:54 -0300

    rating e prestador

--- web/pages/prestador/index.js
+++ web/pages/prestador/index.js
@@ -6,9 +6,10 @@ import Button from '../../src/components/Button';
 import Galeria from '../../src/components/Galeria';
 import IconeTexto from '../../src/components/IconeETexto';
 import Navbar from '../../src/components/Navbar';
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelopeOpen, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelopeOpen, faHighlighter, faStar } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
   faEnvelopeOpen,
@@ -19,20 +20,23 @@ library.add(
 
 
 
-export default function Estabelecimento() {
+
+
+
+export default function Prestador() {
   
   let imgArray =  ['https://picsum.photos/500/300', 'https://picsum.photos/500/300', 'https://picsum.photos/200/300', 'https://picsum.photos/200/300', 'https://picsum.photos/200/300', 'https://picsum.photos/200/300'];
   let mapsLink = `https://maps.google.com/maps?q=35.856737, 10.606619&z=15&output=embed`;
-
+  
   return (
     <>
   
     
     <Galeria imgArray={imgArray}></Galeria>
     <div className="nome-cliente prestador-titulo">
-         <h2>Hotel de tangamandápio</h2>
+         <h2>João da silva sauro</h2>
          <div className="rating">
-              4,9
+              4,0
               <FontAwesomeIcon icon={faStar} style={{color: 'red'}} />
          </div>
     </div>
@@ -40,7 +44,7 @@ export default function Estabelecimento() {
     <Navbar></Navbar>
     <IconeTexto icone={faEnvelopeOpen} text="Atributo de teste"></IconeTexto>
     <div className="button-container">
-         <Button type="link" text="Fazer reserva" href="/checkout?="></Button>
+         <Button type="link" text="Contratar" href="/checkout?="></Button>
     </div>
 
 
