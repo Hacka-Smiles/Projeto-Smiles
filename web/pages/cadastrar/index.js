7b96ecead7ba76840e723c16a8652856882cabef
ViniciusSerafim <vinicmatheus@usp.br>
2021-03-28 20:15:50 -0300

    Cadastrar

--- web/pages/cadastrar/index.js
+++ web/pages/cadastrar/index.js
@@ -4,6 +4,13 @@ import { ThemeProvider } from "styled-components";
 import db from "../../db.json";
 import Button from "../../src/components/Button";
 
+import Input from '../../src/components/Input';
+import styled from 'styled-components'
+
+import IconeTexto from '../../src/components/IconeETexto';
+import { faEnvelopeOpen, faLock } from '@fortawesome/free-solid-svg-icons';
+import Navbar from '../../src/components/Navbar';
+
 export default function Cadastrar() {
   // const [userName, setUsername] = useState("");
   // const [usuario, setUsuario] = useState("");
@@ -18,8 +25,27 @@ export default function Cadastrar() {
   };
 
   return (
-    <ThemeProvider theme={db.theme}>
-      <Button type="postClient" params={obj} href="/checkout"/>
-    </ThemeProvider>
+      <>
+      <div  className="capa-login">
+          <img src="capa-login.jpg"></img>
+          <h3>NomeDaEmpresa</h3>
+          <h4>Slogan da emppresa</h4>
+      </div>
+        <div className="form-container">
+        <Input icone={faEnvelopeOpen} placeholder="Digite aqui o seu nome" type="text"/>
+        <Input icone={faEnvelopeOpen} placeholder="Digite aqui a sua idade" type="text"/>
+        <Input icone={faEnvelopeOpen} placeholder="Digite aqui o seu CPF" type="text"/>
+        <Input icone={faEnvelopeOpen} placeholder="Digite aqui o seu e-mail" type="text"/>
+        <Input icone={faLock} placeholder="Digite aqui a sua senha" type="text"/>
+
+
+
+        <Input icone={faLock} placeholder="Digite aqui a sua senha novamente" type="password"/>
+
+        </div>
+        <div className="button-container">
+        <Button text="enviar" href="/checkout" type="link"></Button>      
+        </div>
+      </>
   );
 }
