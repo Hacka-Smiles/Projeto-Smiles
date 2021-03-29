b24a5507f6f0f3721b79b2b1d1bf27a060e7a9b5
ViniciusSerafim <vinicmatheus@usp.br>
2021-03-28 21:01:35 -0300

    commit de testezi

--- web/pages/cadastrar/index.js
+++ web/pages/cadastrar/index.js
@@ -35,6 +35,23 @@ export default function Cadastrar() {
         <Input icone={faEnvelopeOpen} placeholder="Digite aqui o seu CPF" type="text"/>
         <Input icone={faEnvelopeOpen} placeholder="Digite aqui o seu e-mail" type="text"/>
         <Input icone={faLock} placeholder="Digite aqui a sua senha" type="text"/>
+        <div class="custom-select" style="width:200px;">
+            <select>
+              <option value="0">Select car:</option>
+              <option value="1">Audi</option>
+              <option value="2">BMW</option>
+              <option value="3">Citroen</option>
+              <option value="4">Ford</option>
+              <option value="5">Honda</option>
+              <option value="6">Jaguar</option>
+              <option value="7">Land Rover</option>
+              <option value="8">Mercedes</option>
+              <option value="9">Mini</option>
+              <option value="10">Nissan</option>
+              <option value="11">Toyota</option>
+              <option value="12">Volvo</option>
+          </select>
+</div>
 
 
 
